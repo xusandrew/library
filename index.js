249a1c2e0fa22746bce82deef618877ad354c063
Andrew Xu <xusandrew@gmail.com>
2023-01-05 21:25:44 -0500

    Add classes and update styling

--- index.js
+++ index.js
@@ -19,27 +19,33 @@ class Library {
     displayBooks() {
         const container = document.getElementById("library")
 
-        this.books.forEach((book, i) => {
+        this.books.forEach(book => {
             let card = document.createElement("div")
+            card.classList.add("card")
 
             let name = document.createElement("h2")
             name.appendChild(document.createTextNode('"' + book.name + '"'))
+            name.classList.add("name")
             card.appendChild(name)
 
             let author = document.createElement("p")
             author.appendChild(document.createTextNode(book.author))
+            author.classList.add("author")
             card.appendChild(author)
 
             let pages = document.createElement("p")
             pages.appendChild(document.createTextNode(book.pages + " pages"))
+            pages.classList.add("pages")
             card.appendChild(pages)
 
             let read_button = document.createElement("button")
             read_button.appendChild(document.createTextNode("Read"))
+            read_button.classList.add("read")
             card.appendChild(read_button)
 
             let remove_button = document.createElement("button")
             remove_button.appendChild(document.createTextNode("Remove"))
+            remove_button.classList.add("remove")
             card.appendChild(remove_button)
 
             container.appendChild(card)
